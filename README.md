9f4921432bfdd8fc8e75c6ce8f82106e052606fb
Antonio Martínez <antonio@weffective.com>
2025-04-17 21:29:36 +0200

    feat: soap bubble operator scaffolding

--- README.md
+++ README.md
@@ -22,3 +22,22 @@
 
 1. https://randomnerdtutorials.com/esp32-vs-code-platformio-littlefs/
 1. https://randomnerdtutorials.com/esp32-wi-fi-manager-asyncwebserver/
+
+## Kubernetes Operator Creation
+
+1. Scaffold the operator with kubebuilder
+
+```bash
+mkdir soap-bubble-operator
+cd soap-bubble-operator
+kubebuilder init --domain soap-bubble-operator.local --repo github.com/AntonioMartinezFernandez/soap-bubble-project/soap-bubble-operator
+kubebuilder create api --group soap-bubble-operator --version v1alpha1 --kind SoapBubbleMachine
+
+# Answer both questions with "yes"
+```
+
+2. Edit the `api/v1alpha1/soapbubblemachine_types.go` file and set the CRD schema
+3. Edit the `config/samples/soap-bubble-operator_v1alpha1_soapbubblemachine.yaml` file and set the sample values for the CR
+4. Execute `make manifests`
+5. Execute `make build`
+6. With the kubernetes cluster running, execute `make install`
---
+++ soap-bubble-operator/api/v1alpha1/soapbubblemachine_types.go
@@ -0,0 +1,68 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// SoapBubbleMachineSpec defines the desired state of SoapBubbleMachine.
+type SoapBubbleMachineSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	MachineName string `json:"machineName,omitempty"`
+	StartURL    string `json:"startURL,omitempty" validate:"url"`
+	StopURL     string `json:"stopURL,omitempty" validate:"url"`
+	State       string `json:"state,omitempty" validate:"oneof=on off"`
+}
+
+// SoapBubbleMachineStatus defines the observed state of SoapBubbleMachine.
+type SoapBubbleMachineStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	State string `json:"state,omitempty" validate:"oneof=on off"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// SoapBubbleMachine is the Schema for the soapbubblemachines API.
+type SoapBubbleMachine struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   SoapBubbleMachineSpec   `json:"spec,omitempty"`
+	Status SoapBubbleMachineStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// SoapBubbleMachineList contains a list of SoapBubbleMachine.
+type SoapBubbleMachineList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []SoapBubbleMachine `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&SoapBubbleMachine{}, &SoapBubbleMachineList{})
+}
