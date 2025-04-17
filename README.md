b659ef6d4aac7f70b79bff0defde81004df9e1b9
Antonio Martínez <antonio@weffective.com>
2025-04-17 20:21:15 +0200

    feat: soap bubble machine firmware with wifi manager

---
+++ README.md
@@ -0,0 +1,24 @@
+# Soap Bubble Project
+
+## Pre-requisites
+
+### Hardware
+
+- ESP32CAM
+- USB-UART adapter
+- USB cable
+- Dupont cables
+
+### Software
+
+- VSCode
+- PlatformIO
+- Kubernetes Cluster (available with Orbstack or Docker Desktop)
+- Golang
+- kubectl
+- kubebuilder
+
+## Resources
+
+1. https://randomnerdtutorials.com/esp32-vs-code-platformio-littlefs/
+1. https://randomnerdtutorials.com/esp32-wi-fi-manager-asyncwebserver/
--- soap-bubble-machine/src/main.cpp
+++ soap-bubble-machine/src/main.cpp
@@ -1,115 +1,272 @@
-#include <Arduino.h>
+/*********
+ *  ESP32 Web Server with LittleFS
+ *  This code sets up a web server on an ESP32 board that allows users
+ *  to configure Wi-Fi settings through a web interface, and control an output pin.
+ *
+ *  Created on: 2025-04-17
+ *********/
 
+#include <Arduino.h>
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
+#include <AsyncTCP.h>
+#include "LittleFS.h"
 
-// WiFi credentials
-#define WIFI_SSID "WIFI_SSID"
-#define WIFI_PASSWORD "WIFI_PASSWORD"
-
-// OUTPUT pin AND state
-#define OUTPUT_PIN 2
-int OUTPUT_STATE = LOW;
-
-// Web server object
+// Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
-// Function to generate the HTML and CSS code for the web page
-String getHTML()
+// Search for parameter in HTTP POST request
+const char *PARAM_INPUT_1 = "ssid";
+const char *PARAM_INPUT_2 = "pass";
+const char *PARAM_INPUT_3 = "ip";
+const char *PARAM_INPUT_4 = "gateway";
+
+// Variables to save values from HTML form
+String ssid;
+String pass;
+String ip;
+String gateway;
+
+// File paths to save input values permanently
+const char *ssidPath = "/ssid.txt";
+const char *passPath = "/pass.txt";
+const char *ipPath = "/ip.txt";
+const char *gatewayPath = "/gateway.txt";
+
+IPAddress localIP;
+// IPAddress localIP(192, 168, 1, 200); // hardcoded
+
+// Set your Gateway IP address
+IPAddress localGateway;
+// IPAddress localGateway(192, 168, 1, 1); //hardcoded
+IPAddress subnet(255, 255, 0, 0);
+
+// Timer variables
+unsigned long previousMillis = 0;
+const long interval = 10000; // interval to wait for Wi-Fi connection (milliseconds)
+
+// Set LED GPIO
+const int outputPin = 4;
+// Stores LED state
+
+String ledState;
+
+// Initialize LittleFS
+void initLittleFS()
 {
-  String html = "<!DOCTYPE HTML>";
-  html += "<html>";
-  html += "<head>";
-  html += "<style>";
-  html += "body {background-color: #F0F0F0; font-family: Arial, Helvetica, sans-serif;}";
-  html += "h1 {color: #333333; text-align: center;}";
-  html += "button {width: 150px; height: 50px; font-size: 20px; margin: 10px;}";
-  html += "</style>";
-  html += "</head>";
-  html += "<body>";
-  html += "<h1>SOAP BUBBLE MACHINE</h1>";
-  html += "<p>OUTPUT state: <span style='color: red;'>";
-  if (OUTPUT_STATE == LOW)
-    html += "OFF";
-  else
-    html += "ON";
-  html += "</span></p>";
-  html += "<button onclick=\"window.location.href='/output/on'\">Turn ON</button>";
-  html += "<button onclick=\"window.location.href='/output/off'\">Turn OFF</button>";
-  html += "</body>";
-  html += "</html>";
-  return html;
+  if (!LittleFS.begin(true))
+  {
+    Serial.println("An error has occurred while mounting LittleFS");
+  }
+  Serial.println("LittleFS mounted successfully");
 }
 
-// Function to connect to WiFi network
-void connectWiFi()
+// Read File from LittleFS
+String readFile(fs::FS &fs, const char *path)
 {
-  Serial.print("Connecting to WiFi...");
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.printf("Reading file: %s\r\n", path);
+
+  File file = fs.open(path);
+  if (!file || file.isDirectory())
+  {
+    Serial.println("- failed to open file for reading");
+    return String();
+  }
+
+  String fileContent;
+  while (file.available())
+  {
+    fileContent = file.readStringUntil('\n');
+    break;
+  }
+  return fileContent;
+}
+
+// Write file to LittleFS
+void writeFile(fs::FS &fs, const char *path, const char *message)
+{
+  Serial.printf("Writing file: %s\r\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if (!file)
+  {
+    Serial.println("- failed to open file for writing");
+    return;
+  }
+  if (file.print(message))
+  {
+    Serial.println("- file written");
+  }
+  else
+  {
+    Serial.println("- write failed");
+  }
+}
+
+// Initialize WiFi
+bool initWiFi()
+{
+  if (ssid == "" || ip == "")
+  {
+    Serial.println("Undefined SSID or IP address.");
+    return false;
+  }
+
+  WiFi.mode(WIFI_STA);
+  localIP.fromString(ip.c_str());
+  localGateway.fromString(gateway.c_str());
+
+  if (!WiFi.config(localIP, localGateway, subnet))
+  {
+    Serial.println("STA Failed to configure");
+    return false;
+  }
+  WiFi.begin(ssid.c_str(), pass.c_str());
+  Serial.println("Connecting to WiFi...");
+
+  unsigned long currentMillis = millis();
+  previousMillis = currentMillis;
+
   while (WiFi.status() != WL_CONNECTED)
   {
-    delay(500);
-    Serial.print(".");
+    currentMillis = millis();
+    if (currentMillis - previousMillis >= interval)
+    {
+      Serial.println("Failed to connect.");
+      return false;
+    }
   }
-  Serial.println();
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+
   Serial.println(WiFi.localIP());
+  return true;
 }
 
-// Function to handle HTTP requests
-void handleRequest(AsyncWebServerRequest *request)
+// Replaces placeholder with LED state value
+String processor(const String &var)
 {
-  // Get the request path
-  String path = request->url();
-  // Check if the request is to turn the OUTPUT on
-  if (path == "/output/on")
+  if (var == "STATE")
   {
-    // Set the OUTPUT pin to HIGH
-    digitalWrite(OUTPUT_PIN, HIGH);
-    // Update the OUTPUT state
-    OUTPUT_STATE = HIGH;
-    // Send a confirmation message
-    request->send(200, "text/plain", "OUTPUT turned on");
+    if (digitalRead(outputPin))
+    {
+      ledState = "ON";
+    }
+    else
+    {
+      ledState = "OFF";
+    }
+    return ledState;
   }
-  // Check if the request is to turn the OUTPUT off
-  else if (path == "/output/off")
-  {
-    // Set the OUTPUT pin to LOW
-    digitalWrite(OUTPUT_PIN, LOW);
-    // Update the OUTPUT state
-    OUTPUT_STATE = LOW;
-    // Send a confirmation message
-    request->send(200, "text/plain", "OUTPUT turned off");
-  }
-  // Otherwise, send the web page
-  else
-  {
-    // Get the HTML and CSS code
-    String html = getHTML();
-    // Send the web page
-    request->send(200, "text/html", html);
-  }
+  return String();
 }
 
 void setup()
 {
-  // Initialize the serial monitor
+  // Serial port for debugging purposes
   Serial.begin(115200);
 
-  // Initialize the OUTPUT pin
-  pinMode(OUTPUT_PIN, OUTPUT);
-  digitalWrite(OUTPUT_PIN, OUTPUT_STATE);
+  initLittleFS();
 
-  // Connect to WiFi network
-  connectWiFi();
+  // Set GPIO 2 as an OUTPUT
+  pinMode(outputPin, OUTPUT);
+  digitalWrite(outputPin, LOW);
 
-  // Start the web server
-  server.onNotFound(handleRequest);
-  server.begin();
+  // Load values saved in LittleFS
+  ssid = readFile(LittleFS, ssidPath);
+  pass = readFile(LittleFS, passPath);
+  ip = readFile(LittleFS, ipPath);
+  gateway = readFile(LittleFS, gatewayPath);
+  Serial.println(ssid);
+  Serial.println(pass);
+  Serial.println(ip);
+  Serial.println(gateway);
+
+  if (initWiFi())
+  {
+    // Route for root / web page
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send(LittleFS, "/index.html", "text/html", false, processor); });
+    server.serveStatic("/", LittleFS, "/");
+
+    // Route to set GPIO state to HIGH
+    server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
+      digitalWrite(outputPin, HIGH);
+      request->send(LittleFS, "/index.html", "text/html", false, processor); });
+
+    // Route to set GPIO state to LOW
+    server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
+      digitalWrite(outputPin, LOW);
+      request->send(LittleFS, "/index.html", "text/html", false, processor); });
+
+    // Route to handle file upload
+    server.begin();
+  }
+  else
+  {
+    // Connect to Wi-Fi network with SSID and password
+    Serial.println("Setting AP (Access Point)");
+    // NULL sets an open Access Point
+    WiFi.softAP("ESP-WIFI-MANAGER", NULL);
+
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP);
+
+    // Web Server Root URL
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send(LittleFS, "/wifimanager.html", "text/html"); });
+
+    server.serveStatic("/", LittleFS, "/");
+
+    server.on("/", HTTP_POST, [](AsyncWebServerRequest *request)
+              {
+      int params = request->params();
+      for(int i=0;i<params;i++){
+        const AsyncWebParameter* p = request->getParam(i);
+        if(p->isPost()){
+          // HTTP POST ssid value
+          if (p->name() == PARAM_INPUT_1) {
+            ssid = p->value().c_str();
+            Serial.print("SSID set to: ");
+            Serial.println(ssid);
+            // Write file to save value
+            writeFile(LittleFS, ssidPath, ssid.c_str());
+          }
+          // HTTP POST pass value
+          if (p->name() == PARAM_INPUT_2) {
+            pass = p->value().c_str();
+            Serial.print("Password set to: ");
+            Serial.println(pass);
+            // Write file to save value
+            writeFile(LittleFS, passPath, pass.c_str());
+          }
+          // HTTP POST ip value
+          if (p->name() == PARAM_INPUT_3) {
+            ip = p->value().c_str();
+            Serial.print("IP Address set to: ");
+            Serial.println(ip);
+            // Write file to save value
+            writeFile(LittleFS, ipPath, ip.c_str());
+          }
+          // HTTP POST gateway value
+          if (p->name() == PARAM_INPUT_4) {
+            gateway = p->value().c_str();
+            Serial.print("Gateway set to: ");
+            Serial.println(gateway);
+            // Write file to save value
+            writeFile(LittleFS, gatewayPath, gateway.c_str());
+          }
+          //Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
+        }
+      }
+      request->send(200, "text/plain", "Done. ESP will restart, connect to your router and go to IP address: " + ip);
+      delay(3000);
+      ESP.restart(); });
+    server.begin();
+  }
 }
 
 void loop()
 {
-  // Nothing to do here
 }
